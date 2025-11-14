09e3762561b3bbbff51cfa0c2a3fad0a9d55f43d
Ajith3204 <officialajith001@gmail.com>
2025-11-14 14:38:10 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SimpleLibrary {
+    public class Calculator : ICalculator {
+        public int Add(int a, int b) {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b) {
+            return a - b;
+        }
+    }
+}
